f4522a8a6ac1d270937bf2e7e3648ed718d7195f
John Yenter-Briars <jyenterbriars@gmail.com>
2023-02-18 14:50:20 -0600

    add qbt interface

--- fullsail.py
+++ fullsail.py
@@ -1,9 +1,13 @@
 from magnetlinkscraper import solidtorrent_search
+from qbittorrentinterface import start_qbittorrentinterface
+
 
 def main():
-    query = "avengers end game"
-    # t1337x_search(query)
-    solidtorrent_search(query)
+    # query = "avengers end game"
+    # # t1337x_search(query)
+    # solidtorrent_search(query)
+    start_qbittorrentinterface()
+
 
 if __name__ == "__main__":
     main()
