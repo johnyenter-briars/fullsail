cd23742838b01424c2f982ea486e5ca8e082c17d
John Yenter-Briars <jyenterbriars@gmail.com>
2023-02-24 09:14:28 -0600

    fix blocking file sending

--- mediatransfer/sendfile.py
+++ mediatransfer/sendfile.py
@@ -3,9 +3,10 @@ from typing import Dict
 import paramiko
 from asyncio import events
 import fsconfig
+import asyncssh
 
 
-def _send_file(file_name_with_dir: str, future):
+async def send_file(file_name_with_dir):  # finalDict
     username = fsconfig.CONFIG["media-system-username"]
     password = fsconfig.CONFIG["media-system-password"]
     ip = fsconfig.CONFIG["media-system-localip"]
@@ -15,28 +16,12 @@ def _send_file(file_name_with_dir: str, future):
     file_name = foo[-1]
     media_system_root = fsconfig.CONFIG["media-system-media-root"]
 
-    ssh = paramiko.SSHClient()
-
-    ssh.load_host_keys(os.path.expanduser(
-        os.path.join("~", ".ssh", "known_hosts")))
-
-    ssh.connect(ip,
-                username=username, password=password)
-
-    sftp = ssh.open_sftp()
-    sftp.put(full_file_path, f"{media_system_root}/{file_name}")
-    sftp.close()
-    ssh.close()
-
-    future.set_result("done")
-
-
-async def send_file(filename):
-    loop = events.get_running_loop()
-    future = loop.create_future()
-    h = loop.call_later(0, _send_file, filename, future)
-
     try:
-        return await future
-    finally:
-        h.cancel()
+        print("Performing SSH Connection to the device")
+        async with asyncssh.connect(ip, username=username, password=password, known_hosts=None) as conn:
+            async with conn.start_sftp_client() as sftp:
+                await sftp.put(full_file_path, f"{media_system_root}/{file_name}")
+
+        print("Channel established")
+    except Exception as e:
+        print(e)
