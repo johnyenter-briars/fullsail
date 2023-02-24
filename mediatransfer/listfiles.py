e8631d94f9850fe817e182cd23bc8ab816146c86
John Yenter-Briars <jyenterbriars@gmail.com>
2023-02-24 08:12:29 -0600

    list files with optionally duration

--- mediatransfer/listfiles.py
+++ mediatransfer/listfiles.py
@@ -5,36 +5,59 @@ from typing import List, Tuple
 from ffprobe import FFProbe
 import fsconfig
 
-VIDEO_EXTENSIONS = {".mkv", ".mp4"}
 
-
-async def list_files():
+async def media_files(get_duration):
     media_root = fsconfig.CONFIG["media-root"]
 
-    return get_media_files_with_duration(media_root)
-
-
-def get_media_files_with_duration(directory) -> List:
     media_files = []
+
+    _get_file_metadata(
+        media_root,  [".mkv", ".mp4", ".avi"], media_files, get_duration)
+
+    return media_files
+
+
+async def subtitle_files():
+    media_root = fsconfig.CONFIG["media-root"]
+
+    media_files = []
+
+    _get_file_metadata(media_root, [".srt"], media_files, False)
+
+    return media_files
+
+
+def _get_file_metadata(directory, extensions, media_files=[], get_duration=True) -> List:
+    media_root = fsconfig.CONFIG["media-root"]
     for file_name in os.listdir(directory):
         file_path = os.path.join(directory, file_name)
-        if os.path.isfile(file_path) and os.path.splitext(file_path)[1].lower() in VIDEO_EXTENSIONS:
-            try:
-                cmd = 'ffprobe -i {} -show_entries format=duration -v quiet -of csv="p=0"'.format(
-                    file_path)
+        if os.path.isfile(file_path):
+            if os.path.splitext(file_path)[1].lower() in extensions:
+                if get_duration:
+                    try:
+                        cmd = f'ffprobe -i "{file_path}" -show_entries format=duration -v quiet -of csv="p=0"'
 
-                output = subprocess.check_output(
-                    cmd,
-                    shell=True,  # Let this run in the shell
-                    stderr=subprocess.STDOUT
-                )
+                        output = subprocess.check_output(
+                            cmd,
+                            shell=True,  # Let this run in the shell
+                            stderr=subprocess.STDOUT
+                        )
 
-                minutes = int(float(output) / 60)
-                media_files.append({
-                    "name": file_name,
-                    "duration": minutes,
-                })
+                        minutes = int(float(output) / 60)
+                        media_files.append({
+                            "name": file_path.replace(media_root, ""),
+                            "duration": minutes,
+                        })
+                    except Exception as e:
+                        print(
+                            f"Failed to extract duration for file {file_name}: {e}")
+                else:
+                    media_files.append({
+                        "name": file_path.replace(media_root, ""),
+                        "duration": None,
+                    })
+        else:
+            _get_file_metadata(file_path, extensions,
+                               media_files, get_duration)
 
-            except Exception as e:
-                print(f"Failed to extract duration for file {file_name}: {e}")
-    return media_files
+    # return media_files
--- webinterface/app.py
+++ webinterface/app.py
@@ -1,6 +1,6 @@
 import asyncio
 from typing import List
-from mediatransfer.listfiles import list_files
+from mediatransfer.listfiles import media_files, subtitle_files
 import fsconfig
 from mediatransfer import send_file
 from aiohttp import web
@@ -11,55 +11,61 @@ import jsonpickle
 from models import StartTorrentsRequest
 from models import UpdateTorrentsRequest
 from models.qbtfile import QBTFile, current_torrents_response_from_dict, current_torrents_response_to_dict
+from distutils.util import strtobool
 
 from qbittorrentinterface.methods import delete_torrent, get_running_torrents, pause_torrent, add_torrent, resume_torrent
 
-futures = []
-future_id = 0
+media_transfer_jobs = []
 
 routes = web.RouteTableDef()
 
 
-@routes.get('/api/job/start')
-async def start_job(request):
-    future = asyncio.create_task(schedule_send_file_job())
+@routes.get('/api/mediatransfer/start/{file_name}')
+async def _start_job(request):
+    future = asyncio.create_task(_schedule_send_file_job())
 
-    futures.append(future)
-
-    global future_id
-    future_id += 1
+    media_transfer_jobs.append(future)
 
     return web.Response(text=f"Future scheduled!")
 
 
-async def schedule_send_file_job():
-    for i in range(10, 20):
-        await send_file(f"test{i}-{future_id}.txt")
+async def _schedule_send_file_job(file_name):
+    await send_file(file_name)
 
-    futures.remove(asyncio.current_task())
+    media_transfer_jobs.remove(asyncio.current_task())
 
 
-@routes.get('/api/jobs/list')
-async def list_jobs(request):
-    return web.Response(text=f"num futures:{len(futures)}")
+@routes.get('/api/mediatransfer/listjobs')
+async def _list_jobs(request):
+    return web.Response(text=f"num futures:{len(media_transfer_jobs)}")
 
 
 @routes.get('/api/media/list')
-async def list_jobs(request):
-    response = await list_files()
+async def _list_media(request):
+    get_duration = request.query["duration"]
+
+    get_duration = bool(strtobool(get_duration))
+
+    response = await media_files(get_duration)
+
+    return web.json_response(response)
+
+@routes.get('/api/media/list/subs')
+async def _list_subs(request):
+    response = await subtitle_files()
 
     return web.json_response(response)
 
 
 @routes.get('/api/torrents/list')
-async def list_torrents(request):
+async def _list_torrents(request):
     files: List[dict] = await get_running_torrents()
 
     return web.json_response(files)
 
 
 @routes.post('/api/torrents/add')
-async def add_torrents(request):
+async def _add_torrents(request):
     r = StartTorrentsRequest.from_dict(await request.json())
 
     for magnet_link in r.magnet_links:
@@ -69,7 +75,7 @@ async def add_torrents(request):
 
 
 @routes.post('/api/torrents/resume')
-async def resume_torrents(request):
+async def _resume_torrents(request):
     r = UpdateTorrentsRequest.from_dict(await request.json())
 
     for hash in r.hashes:
@@ -89,7 +95,7 @@ async def pause_torrents(request):
 
 
 @routes.post('/api/torrents/delete')
-async def delete_torrents(request):
+async def _delete_torrents(request):
     r = UpdateTorrentsRequest.from_dict(await request.json())
 
     for hash in r.hashes:
@@ -99,7 +105,7 @@ async def delete_torrents(request):
 
 
 @routes.get('/api/search/{torrent_site}/{search_term}')
-async def search_torrents(request):
+async def _search_torrents(request):
     search_term = request.match_info['search_term']
     torrent_site = request.match_info['torrent_site']
 
