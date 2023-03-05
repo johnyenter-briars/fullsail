2f1200503edb6fa39b8a589afdd31b13f500f4be
John Yenter-Briars <jyenterbriars@gmail.com>
2023-03-05 14:04:12 -0600

    initial poc for both caching and folder searching

--- webinterface/app.py
+++ webinterface/app.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 import logging
 from typing import List
@@ -10,6 +11,7 @@ from magnetlinkscraper import solidtorrent_search
 from distutils.util import strtobool
 from concurrent.futures import ThreadPoolExecutor
 from mediatransfer.deletefilemediasystem import delete_file_mediasystem
+from mediatransfer.listfiles import list_media_files_in_folder, list_sub_files_in_folder
 from mediatransfer.listfilesmediasystem import list_files_mediasystem
 
 from qbittorrentinterface import delete_torrent, get_running_torrents, pause_torrent, add_torrent, resume_torrent
@@ -25,7 +27,8 @@ async def _start_job(request):
 
     file_name = r["fileName"]
 
-    media_transfer_jobs.append((asyncio.create_task(send_file(file_name)), file_name))
+    media_transfer_jobs.append(
+        (asyncio.create_task(send_file(file_name)), file_name))
 
     return web.json_response({
         "numberRunningJobs": len(media_transfer_jobs)
@@ -45,21 +48,58 @@ async def _list_jobs(request):
     return web.json_response({"message": "jobs found", "jobs": files})
 
 
+async def update_cache():
+    with open('./mediacache/cache.json', 'w') as cache_file:
+        current_cache = fsconfig.MEDIACACHE
+        files_to_ignore = [file
+                           for file in current_cache["media-root"]]
+
+        new_file_list = await media_files(True, files_to_ignore)
+
+        for new_file in new_file_list:
+            target_files = list(filter(lambda f: f["name"] == new_file["name"] ,fsconfig.MEDIACACHE["media-root"]))
+
+            if  len(target_files) != 0:
+                target_file = target_files[0]
+                target_file["duration"] = new_file["duration"]
+            else:
+                fsconfig.MEDIACACHE["media-root"].append(new_file)
+
+        cache_file.write(json.dumps(fsconfig.MEDIACACHE))
+
+@routes.get('/api/media/list/{folder}')
+async def _list_media_in_folder(request):
+    folder = request.match_info['folder']
+
+    files = await list_media_files_in_folder(folder)
+
+    return web.json_response({"message": "files found", "files": files})
+
+@routes.get('/api/subs/list/{folder}')
+async def _list_media_in_folder(request):
+    folder = request.match_info['folder']
+
+    files = await list_sub_files_in_folder(folder)
+
+    return web.json_response({"message": "files found", "files": files})
+
+
 @routes.get('/api/media/list')
 async def _list_media(request):
-    get_duration = request.query["duration"]
+    media_transfer_jobs.append(
+        (asyncio.create_task(update_cache()), "updating cache"))
 
-    get_duration = bool(strtobool(get_duration))
+    cache = fsconfig.MEDIACACHE
 
-    response = await media_files(get_duration)
+    return web.json_response(cache)
 
-    return web.json_response(response)
 
 @routes.get('/api/media-system/list')
 async def _list_medimediasystem(request):
     response = await list_files_mediasystem()
 
     return web.json_response(response)
+
 
 @routes.delete('/api/media-system/delete')
 async def _delete_files_mediasystem(request):
