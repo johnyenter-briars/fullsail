5555f782bfab1e2c58d53fb212a1864f10b0e421
John Yenter-Briars <jyenterbriars@gmail.com>
2023-03-05 17:00:57 -0600

    more remove cache

--- fullsail.py
+++ fullsail.py
@@ -4,12 +4,9 @@ from webinterface import start_webinterface
 
 
 def main():
-    with open('config.json') as f, \
-            open('./mediacache/cache.json') as c:
+    with open('config.json') as f:
         config = json.load(f)
-        cache = json.load(c)
         fsconfig.CONFIG = {key: value for key, value in config.items()}
-        fsconfig.MEDIACACHE = {key: value for key, value in cache.items()}
 
     start_webinterface(config)
 
