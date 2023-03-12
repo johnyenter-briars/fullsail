2e007517fab09186a66f0d59e04d3b251e094ab9
John Yenter-Briars <jyenterbriars@root16.com>
2023-03-12 17:23:14 -0500

    add ff and rewind functionality + power

--- app/FullSail/FullSail/ViewModels/MediaControlViewModel.cs
+++ app/FullSail/FullSail/ViewModels/MediaControlViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -31,8 +32,8 @@ namespace FullSail.ViewModels
                 await KodiClientSingleton.PowerOff();
             }
         });
-        public ICommand FastForwardCommand => new Command<Button>(async (Button button) => { await KodiClientSingleton.PowerReboot(); });
-        public ICommand RewindCommand => new Command<Button>(async (Button button) => { await KodiClientSingleton.PowerReboot(); });
+        public ICommand RewindCommand => new Command<Button>(async (Button button) => { await KodiClientSingleton.SetPlayerSpeed(DecSpeed()); });
+        public ICommand FastFowardCommand => new Command<Button>(async (Button button) => { await KodiClientSingleton.SetPlayerSpeed(IncSpeed()); });
         private string searchText = "";
 
         public string SearchText
@@ -49,5 +50,54 @@ namespace FullSail.ViewModels
         {
             await KodiClientSingleton.InputText(searchText);
         });
+        public async Task SetFastFoward()
+        {
+
+        }
+        public async Task SetRewind()
+        {
+            await KodiClientSingleton.SetPlayerSpeed(DecSpeed());
+        }
+        private int currSpeed = 0;
+        private int MAX_FF_SPEED = 8;
+        private int MAX_RR_SPEED = -8;
+
+        public void SetSpeed(int s)
+        {
+            currSpeed = s;
+        }
+
+        public int IncSpeed()
+        {
+            if (currSpeed <= 1)
+            {
+                currSpeed = 2;
+            }
+            else if (currSpeed != MAX_FF_SPEED)
+            {
+                currSpeed += 2;
+            }
+
+            return currSpeed;
+        }
+
+        public int DecSpeed()
+        {
+            if (currSpeed >= 1)
+            {
+                currSpeed = -2;
+            }
+            else if (currSpeed != MAX_RR_SPEED)
+            {
+                currSpeed -= 2;
+            }
+
+            return currSpeed;
+        }
+
+        public int GetSpeed()
+        {
+            return currSpeed;
+        }
     }
 }
--- app/FullSail/FullSail/Views/MediaControlPage.xaml.cs
+++ app/FullSail/FullSail/Views/MediaControlPage.xaml.cs
@@ -1,11 +1,14 @@
 using AlohaKit.Animations;
 using FullSail.ViewModels;
+using System.Diagnostics;
 
 namespace FullSail.Views;
 
 public partial class MediaControlPage : ContentPage
 {
     private KodiClient kodiClient;
+    IDispatcherTimer timer;
+    Stopwatch stopwatch = new Stopwatch();
     public MediaControlPage()
     {
         InitializeComponent();
@@ -13,7 +16,42 @@ public partial class MediaControlPage : ContentPage
         BindingContext = new MediaControlViewModel();
 
         kodiClient = DependencyService.Get<KodiClient>();
+
+        timer = Dispatcher.CreateTimer();
+        timer.Interval = TimeSpan.FromMilliseconds(16);
+        //timer.Tick += (s, e) =>
+        //{
+        //    label.Rotation = 360 * (stopwatch.Elapsed.TotalSeconds % 1);
+        //};
     }
+    //void OnFastFowardButtonPressed(object sender, EventArgs args)
+    //{
+    //    var viewModel = (MediaControlViewModel)BindingContext;
+    //    Task.Run(async () =>
+    //    {
+    //        await viewModel.SetFastFoward();
+    //    });
+    //}
+
+    //void OnFastForwardButtonReleased(object sender, EventArgs args)
+    //{
+    //    var viewModel = (MediaControlViewModel)BindingContext;
+    //    viewModel.TogglePlayPauseCommand.Execute(null);
+    //}
+    //void OnRewindButtonPressed(object sender, EventArgs args)
+    //{
+    //    var viewModel = (MediaControlViewModel)BindingContext;
+    //    Task.Run(async () =>
+    //    {
+    //        await viewModel.SetRewind();
+    //    });
+    //}
+
+    //void OnRewindButtonReleased(object sender, EventArgs args)
+    //{
+    //    var viewModel = (MediaControlViewModel)BindingContext;
+    //    await viewModel.TogglePlayPauseCommand();
+    //}
 
     private void VolumeSlider_Changed(object sender, ValueChangedEventArgs e)
     {
