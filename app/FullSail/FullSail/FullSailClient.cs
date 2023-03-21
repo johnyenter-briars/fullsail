d1ce7d50e1b11fb33f1bf9adce0400fbf07e0db8
John Yenter-Briars <jyenterbriars@root16.com>
2023-03-21 13:49:03 -0500

    allow https connections to self signed cert

--- app/FullSail/FullSail/FullSailClient.cs
+++ app/FullSail/FullSail/FullSailClient.cs
@@ -17,7 +17,11 @@ public class FullSailClient
     private string _apiKey = "";
     private string _hostName = "";
     private int _port = 8082;
-    private static readonly HttpClient _httpClient = new()
+    private static HttpClientHandler Handler = new HttpClientHandler()
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    private static readonly HttpClient _httpClient = new HttpClient(Handler)
     {
         Timeout = new TimeSpan(0, 1, 100),
     };
@@ -36,18 +40,18 @@ public class FullSailClient
     }
     private async Task<TResponse> FullSailRequest<TResponse>(string path, HttpMethod httpMethod)
     {
-        var request = new HttpRequestMessage(httpMethod, $"http://{_hostName}:{_port}/api/" + path);
+        var request = new HttpRequestMessage(httpMethod, $"https://{_hostName}:{_port}/api/" + path);
         request.Headers.Accept.Clear();
-        //request.Headers.Add("x-api-key", _apiKey);
+        request.Headers.Add("x-api-key", _apiKey);
         //request.Headers.Add("x-user-id", _userId);
 
         return await SendRequest<TResponse>(request);
     }
     private async Task<TResponse> FullSailRequest<TRequest, TResponse>(TRequest requestObject, string path, HttpMethod httpMethod)
     {
-        var request = new HttpRequestMessage(httpMethod, $"http://{_hostName}:{_port}/api/" + path);
+        var request = new HttpRequestMessage(httpMethod, $"https://{_hostName}:{_port}/api/" + path);
         request.Headers.Accept.Clear();
-        //request.Headers.Add("x-api-key", _apiKey);
+        request.Headers.Add("x-api-key", _apiKey);
         //request.Headers.Add("x-user-id", _userId);
 
         request.Content = new StringContent(JsonConvert.SerializeObject(requestObject, JsonSettings), Encoding.UTF8, "application/json");
@@ -56,6 +60,7 @@ public class FullSailClient
     }
     private async Task<TResponse> SendRequest<TResponse>(HttpRequestMessage request)
     {
+
         var clientResponse = await _httpClient.SendAsync(request, CancellationToken.None);
 
         if (clientResponse.IsSuccessStatusCode)
@@ -68,7 +73,6 @@ public class FullSailClient
             var message = $"Failure to complete action. Reason phrase: {clientResponse.ReasonPhrase}, Raw response: {await clientResponse.Content.ReadAsStringAsync()}, Endpoint: {request.RequestUri}";
             throw new Exception(message);
         }
-
     }
     public async Task<List<MediaFile>> GetMediaFilesInStore(bool getDuration = false)
     {
